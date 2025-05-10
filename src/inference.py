831d67479a8929a3d06921988dbd7b7966a8738f
yaseensiddiqui36 <yaseensiddiqui36@gmail.com>
2025-05-10 15:37:32 -0400

    errors_corrected

--- src/inference.py
+++ src/inference.py
@@ -132,19 +132,19 @@ def fetch_predictions(hours):
 #     return query.read()
 
 
-# def fetch_days_data(days):
-#     current_date = pd.to_datetime(datetime.now(timezone.utc))
-#     fetch_data_from = current_date - timedelta(days=(365 + days))
-#     fetch_data_to = current_date - timedelta(days=365)
-#     print(fetch_data_from, fetch_data_to)
-#     fs = get_feature_store()
-#     fg = fs.get_feature_group(name=config.FEATURE_GROUP_NAME, version=1)
+def fetch_days_data(days):
+    current_date = pd.to_datetime(datetime.now(timezone.utc))
+    fetch_data_from = current_date - timedelta(days=(365 + days))
+    fetch_data_to = current_date - timedelta(days=365)
+    print(fetch_data_from, fetch_data_to)
+    fs = get_feature_store()
+    fg = fs.get_feature_group(name=config.FEATURE_GROUP_NAME, version=1)
 
-#     query = fg.select_all()
-#     # query = query.filter((fg.pickup_hour >= fetch_data_from))
-#     df = query.read()
-#     cond = (df["pickup_hour"] >= fetch_data_from) & (df["pickup_hour"] <= fetch_data_to)
-#     return df[cond]
+    query = fg.select_all()
+    # query = query.filter((fg.pickup_hour >= fetch_data_from))
+    df = query.read()
+    cond = (df["pickup_hour"] >= fetch_data_from) & (df["pickup_hour"] <= fetch_data_to)
+    return df[cond]
 
 
 
